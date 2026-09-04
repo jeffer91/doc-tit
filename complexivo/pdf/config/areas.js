9eef6bfb9f6958cd7e3cbfeb21f941f65dab03b0
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-04 14:59:44 -0500

    Add institutional areas config for Complexivo

---
+++ complexivo/pdf/config/areas.js
@@ -0,0 +1,22 @@
+(() => {
+  "use strict";
+  const ns = window.DOC_TIT_COMPLEXIVO_PDF = window.DOC_TIT_COMPLEXIVO_PDF || {};
+  ns.config = ns.config || {};
+  ns.config.areas = {
+    titulacion: "Titulación y Eficiencia Terminal",
+    coordinacionAcademica: "Coordinación Académica",
+    coordinacionGeneral: "Coordinación General de Carreras",
+    secretaria: "Secretaría Académica",
+    carreras: "Coordinaciones de Carrera",
+    gestionDidactica: "Gestión Didáctica, Diseño Curricular y Calidad Docente",
+    vinculacion: "Vinculación con la Sociedad",
+    practicas: "Prácticas Preprofesionales",
+    bienestar: "Bienestar y Seguimiento Estudiantil",
+    idiomas: "Coordinación Idiomas",
+    recaudacion: "Recaudación y Cartera",
+    ti: "Coordinación de Tecnología de la Información",
+    desarrolloSistemas: "Unidad de Desarrollo de Sistemas",
+    soporteTecnologico: "Infraestructura y Soporte Tecnológico",
+    infraestructuraFisica: "Infraestructura Física y Servicios Generales"
+  };
+})();
