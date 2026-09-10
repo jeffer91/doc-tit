d9c8c1b8ccd995482496d4f60ef87522857f145b
jeffer91 <jeffersonvillarreal91@gmail.com>
2026-09-10 12:45:59 -0500

    Extend shared RGI and APA document standards

--- shared/pdf/standards.js
+++ shared/pdf/standards.js
@@ -5,6 +5,8 @@
   const baseRgi = Object.freeze({
     headerWidthCm: 18,
     headerColumnsCm: Object.freeze([4.5, 9, 4.5]),
+    headerTopCm: 1.5,
+    headerHeightCm: 2.8,
     headerUnitPt: 9,
     headerDocumentPt: 9,
     headerPeriodPt: 9,
@@ -16,11 +18,26 @@
     coverPageNumber: false
   });
 
+  const academic = Object.freeze({
+    bodyFont: "times",
+    bodyFontPt: 12,
+    lineHeightPt: 24,
+    marginLeftCm: 2.54,
+    marginRightCm: 2.54,
+    paragraphIndentCm: 1.27,
+    referenceHangingCm: 1.27,
+    justifyInstitutionalBody: true,
+    bibliographyAlphabetical: true
+  });
+
   window.DOC_TIT_PDF_STANDARDS = Object.freeze({
     page: Object.freeze({ format: "a4", orientation: "portrait", marginCm: 1.5, usableWidthCm: 18 }),
     rgi: baseRgi,
+    academic,
     documentOverrides: Object.freeze({
-      complexivo: Object.freeze({ centralTitlePt: 23 })
+      complexivo: Object.freeze({ centralTitlePt: 23 }),
+      "trabajo-titulacion": Object.freeze({ centralTitlePt: 23 }),
+      "articulo-academico": Object.freeze({ centralTitlePt: 23 })
     }),
     tables: Object.freeze({
       bodyFontPt: 10,
